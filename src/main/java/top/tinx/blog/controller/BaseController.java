a5b06c36db447ce609943efed87a57d1f63064c3
Administrator <loveing490@qq.com>
2019-08-08 21:55:40 +0800

    后台页面继续扩充

--- src/main/java/top/tinx/blog/controller/BaseController.java
+++ src/main/java/top/tinx/blog/controller/BaseController.java
@@ -41,6 +41,36 @@ public class BaseController {
         return "background/artical/writeArtical";
     }
 
-    @GetMapping("manageArtical")
+    @GetMapping("/manageArtical")
     public String manageArtical(){ return "background/artical/manageArtical";}
+
+    @GetMapping("/backBase")
+    public String backBase(){ return "background/info";}
+
+    @GetMapping("/pageManage")
+    public String pageManage(){ return "background/page/pageManage";}
+
+    @GetMapping("/categoryManage")
+    public String categoryManage(){ return "background/page/categoryManage";}
+
+    @GetMapping("/commonManage")
+    public String commonManage(){ return "background/comment/commonManage";}
+
+    @GetMapping("/setArticalStatus")
+    public String setArticalStatus(){ return "background/artical/setArticalStatus";}
+
+    @GetMapping("/userInfo")
+    public String userInfo(){ return "background/user/userInfo";}
+
+    @GetMapping("/password")
+    public String password(){ return "background/user/password";}
+
+    @GetMapping("/auth")
+    public String auth(){ return "background/auth/setAuth";}
+
+    @GetMapping("/websiteSettings")
+    public String websiteSettings(){ return "background/website/websiteSettings";}
+
+    @GetMapping("/fileManage")
+    public String fileManage(){ return "background/file/fileManage";}
 }
